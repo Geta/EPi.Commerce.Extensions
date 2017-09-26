0f2a9d8b1bb3911495ff2d8306dc53cdd0b21547
Māris Krivtežs <marisks@gmail.com>
2017-09-26 12:53:19 +0300

    Updated product extensions to use new relation APIs.

--- src/Geta.EPi.Commerce.Extensions/ProductContentExtensions.cs
+++ src/Geta.EPi.Commerce.Extensions/ProductContentExtensions.cs
@@ -10,18 +10,18 @@ namespace Geta.EPi.Commerce.Extensions
     public static class ProductContentExtensions
     {
         #pragma warning disable 649
-        private static Injected<ILinksRepository> _linksRepository;
+        private static Injected<IRelationRepository> _relationRepository;
         #pragma warning restore 649
 
         /// <summary>
         /// Gets all variants/SKUs for a product
         /// </summary>
         /// <param name="productContent">The product</param>
-        /// <param name="linksRepository">The links repository</param>
+        /// <param name="relationRepository">The relation repository</param>
         /// <returns>Collection of variation references</returns>
-        public static IEnumerable<ContentReference> GetVariations(this ProductContent productContent, ILinksRepository linksRepository)
+        public static IEnumerable<ContentReference> GetVariations(this ProductContent productContent, IRelationRepository relationRepository)
         {
-            return linksRepository.GetRelationsBySource<ProductVariation>(productContent.ContentLink).Select(r => r.Source);
+            return relationRepository.GetChildren<ProductVariation>(productContent.ContentLink).Select(r => r.Child);
         }
 
         /// <summary>
@@ -31,7 +31,7 @@ namespace Geta.EPi.Commerce.Extensions
         /// <returns>Collection of variation references</returns>
         public static IEnumerable<ContentReference> GetVariations(this ProductContent productContent)
         {
-            return productContent.GetVariations(_linksRepository.Service);
+            return productContent.GetVariations(_relationRepository.Service);
         }
     }
 }
