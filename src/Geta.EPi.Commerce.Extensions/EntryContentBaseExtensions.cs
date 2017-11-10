7dc773da7b6482972cec38948dc798d6ae7e0e80
Māris Krivtežs <marisks@gmail.com>
2017-11-10 16:47:38 +0200

    Added extension methods to get ancestor categories of entry.

--- src/Geta.EPi.Commerce.Extensions/EntryContentBaseExtensions.cs
+++ src/Geta.EPi.Commerce.Extensions/EntryContentBaseExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using EPiServer;
 using EPiServer.Commerce.Catalog.ContentTypes;
 using EPiServer.Commerce.Catalog.Linking;
 using EPiServer.Core;
@@ -11,6 +12,7 @@ namespace Geta.EPi.Commerce.Extensions
     {
 #pragma warning disable 649
         private static Injected<IRelationRepository> _relationRepository;
+        private static Injected<IContentLoader> _contentLoader;
 #pragma warning restore 649
 
         /// <summary>
@@ -74,6 +76,47 @@ namespace Geta.EPi.Commerce.Extensions
         public static IEnumerable<ContentReference> GetParentCategories(this EntryContentBase entryContent)
         {
             return entryContent.GetParentCategories(_relationRepository.Service);
+        }
+
+        /// <summary>
+        /// Get ancestor categories.
+        /// </summary>
+        /// <param name="entryContent">The entry content.</param>
+        /// <returns>Collection of category content.</returns>
+        public static IEnumerable<NodeContent> GetAncestorCategories(
+            this EntryContentBase entryContent)
+        {
+            return entryContent.GetAncestorCategories(_contentLoader.Service);
+        }
+
+        /// <summary>
+        /// Get ancestor categories.
+        /// </summary>
+        /// <param name="entryContent">The entry content.</param>
+        /// <param name="contentLoader">The content loader.</param>
+        /// <returns>Collection of category content.</returns>
+        public static IEnumerable<NodeContent> GetAncestorCategories(
+            this EntryContentBase entryContent, IContentLoader contentLoader)
+        {
+            return entryContent.GetCategories()
+                .SelectMany(parentLink =>
+                    contentLoader.GetAncestors(parentLink)
+                        .OfType<NodeContent>()
+                        .Concat(new[] { contentLoader.Get<NodeContent>(parentLink) }))
+                .Distinct(new NodeContentComparer());
+        }
+
+        private class NodeContentComparer : IEqualityComparer<NodeContent>
+        {
+            public bool Equals(NodeContent x, NodeContent y)
+            {
+                return x != null && y != null && x.Code == y.Code;
+            }
+
+            public int GetHashCode(NodeContent obj)
+            {
+                return obj.Code.GetHashCode();
+            }
         }
     }
 }
